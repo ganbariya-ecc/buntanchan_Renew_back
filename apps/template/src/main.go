9ab651c067c8d7a0524a3aa25d3cdbcb71e97ac1
mattuu0 <tempacountyt@gmail.com>
2024-08-11 22:19:35 +0900

    gin から echo に変更

--- apps/template/src/main.go
+++ apps/template/src/main.go
@@ -1,17 +1,14 @@
 package main
 
 import (
-	"net/http"
+	"template/router"
 
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	router := gin.Default()
-	router.GET("/ping", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
-	router.Run(":3001") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	// ルーター初期化
+	router := router.InitRouter()
+
+	// Start server
+	router.Logger.Fatal(router.Start("0.0.0.0:3000"))
 }
---
+++ auth/src/router/router.go
@@ -0,0 +1,22 @@
+package router
+
+import (
+	"auth/controller"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func InitRouter() (*echo.Echo) {
+	// Echo instance
+	router := echo.New()
+
+	// Middleware
+	router.Use(middleware.Logger())
+	router.Use(middleware.Recover())
+
+	// Routes
+	router.GET("/", controller.Hello)
+
+	return router
+}
