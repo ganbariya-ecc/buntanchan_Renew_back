87e50a11208bf4c50e5faaddb5da363e8fae7c0b
mattuu0 <tempacountyt@gmail.com>
2024-09-04 18:33:38 +0900

    template に sdk を追加

---
+++ apps/template/src/sdk/auth.go
@@ -0,0 +1,36 @@
+package sdk
+
+import (
+	"context"
+	"errors"
+	"log"
+	"test/sdk/protoc"
+)
+
+func Auth(token string) (protoc.User,error) {
+	// 初期化済みでない場合 panic
+	if !isInit {
+		log.Fatalln("Not initialized")
+	}
+
+	// コンテキスト生成
+	ctx := context.Background()
+
+	// トークンを渡してユーザーを取得する
+	result, err := gaclient.Auth(ctx, &protoc.AuthData{
+		Token: token,
+	})
+
+	// エラー処理
+	if err != nil {
+		return protoc.User{},err
+	}
+
+	// 成功したか
+	if result.Success {
+		// 成功した場合
+		return *result.User,nil
+	}
+
+	return protoc.User{},errors.New("User authentication failed")
+}
---
+++ apps/template/src/sdk/grpc.go
@@ -0,0 +1,56 @@
+package sdk
+
+import (
+	"log"
+
+	"test/sdk/protoc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+)
+
+var (
+	// GRPC 接続
+	gconn *grpc.ClientConn = nil
+
+	// 認証用クライアント
+	gaclient protoc.AuthServiceClient = nil
+
+	// 初期化済みか
+	isInit = false
+)
+
+func Init() {
+	addr := "auth:9000"
+	// TLS認証を追加
+	creds, err := credentials.NewClientTLSFromFile("server.crt", "")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 接続作成
+	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(creds))
+	// conn, err := grpc.Dial(addr, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+
+	// クライアント作成
+	aclient := protoc.NewAuthServiceClient(conn)
+
+	// グローバル変数に格納
+	gaclient = aclient
+	gconn = conn
+
+	isInit = true
+}
+
+func Disconnect() error {
+	// 初期化済みでない場合 panic
+	if !isInit {
+		log.Fatalln("Not initialized")
+	}
+
+	// 接続を切る
+	return gconn.Close()
+}
+
