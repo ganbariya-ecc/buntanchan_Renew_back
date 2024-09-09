07d1e799aa5efe61c5a9241991c7be1b7ef82231
mattuu0 <tempacountyt@gmail.com>
2024-09-09 23:48:29 +0900

    ユーザーとしてログインを追加

--- auth/src/controller/userc_controller.go
+++ auth/src/controller/userc_controller.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"auth/service"
+	"auth/utils"
 	"log"
 	"net/http"
 
@@ -38,5 +39,40 @@ func UserInfo(ctx echo.Context) error {
 
 	return ctx.JSON(http.StatusOK,echo.Map{
 		"user" : user_data,
+	})
+}
+
+func LoginAS(ctx echo.Context) error {
+	// adminid 取得
+	// adminid := ctx.Get("adminid").(string)
+
+	// ユーザーID
+	userid := ctx.Request().Header.Get("userid")
+
+	// ユーザーとして　ログイン
+	userData,err := service.GetUserInfo(userid)
+
+	// エラー処理
+	if err != nil {
+		log.Println("failed to get user : " + err.Error())
+		return ctx.JSON(http.StatusInternalServerError,echo.Map{
+			"result" : "failed to get user",
+		})
+	}
+
+	// セッション保存
+	err = utils.SetAuth(ctx, "userid", userData.UserID)
+	if err != nil {
+		return err
+	}
+
+	// 認証済みにする
+	err = utils.SetAuth(ctx, "authorized", true)
+	if err != nil {
+		return err
+	}
+
+	return ctx.JSON(http.StatusOK,echo.Map{
+		"result" : "Success",
 	})
 }
